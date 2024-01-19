a37a3f21c753dcdcc033790b923ced329142bed6
nouha404 <aboubacar.marega@ism.edu.sn>
2024-01-19 10:19:13 +0100

    Hello Jupiter

---
+++ src/main/java/com/ism/ismecom/IsmEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ism.ismecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsmEcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ism/ismecom/IsmEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ism.ismecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
